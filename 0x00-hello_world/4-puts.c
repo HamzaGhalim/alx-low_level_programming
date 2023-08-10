f018511797008f1a6ad1e3e10d89a7d322c028e5
HamzaGhalim <hamza.ghalim267@gmail.com>
2023-08-10 15:25:14 -0700

    x

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ *
+ * 
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle\n");
+    return (0);
+}
