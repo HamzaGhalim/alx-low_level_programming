52250d71791981c98b6e934eebcffdd17889e552
HamzaGhalim <hamza.ghalim267@gmail.com>
2023-08-10 15:31:43 -0700

    x

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - A program that prints a line with puts function
  *
- * 
+ * Return: 0 (Success) 
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+put("\"Programming is like building a multilingual puzzle");
+return (0);
 }
